83936c5c6fc50d7798703b081ce82ad88eea196d
Uehara takahiro <takahiro.steadtler@gmail.com>
2019-12-18 01:36:33 +0900

    notebookの準備

--- childrenLearn/train.py
+++ childrenLearn/train.py
@@ -1,4 +1,7 @@
 import numpy as np
-import pandas as import pd
+import pandas as pd
 
-train
+train_x = pd.read_csv('/data/train.csv')
+test_x = pd.read_csv('/data/test.csv')
+
+# https://yolo-kiyoshi.com/2018/12/17/post-1003/ここを参考に書いていく
